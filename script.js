1b118caffe4bb115a16503015e83ae7df32d25c7
unknown <senleodaaauwa@gmail.com>
2026-02-24 12:59:43 +0900

    .15

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const noticeButton = document.querySelector(".button--primary");
 const modal = document.getElementById("noticeModal");
-const closeButton = document.querySelector(".modal__close");
-const overlay = document.querySelector(".modal__overlay");
+const modalClose = document.querySelector(".modal__close");
+const modalOverlay = document.querySelector(".modal__overlay");
+const noticeButton = document.querySelector(".button--primary");
 
 const bgm = document.getElementById("bgm");
 const soundToggle = document.querySelector(".sound-toggle");
 
-const trigger = document.getElementById('menu-trigger');
+const menuTrigger = document.getElementById('menu-trigger');
 const nav = document.getElementById('header-nav');
+const menuOverlay = document.querySelector('.menu-overlay');
 
 bgm.volume = 0.07;
 let isPlaying = false;
@@ -29,14 +30,25 @@ document.querySelector('.menu-trigger').addEventListener('click', function() {
     document.querySelector('.header__nav').classList.toggle('is-open');
 });
 
-noticeButton.addEventListener("click", () => {
-		modal.classList.add("is-open");
+noticeButton.addEventListener('click', () => {
+    modal.classList.add('is-open');
 });
 
-closeButton.addEventListener("click", () => {
-		modal.classList.remove("is-open");
+modalClose.addEventListener('click', () => {
+    modal.classList.remove('is-open');
+});
+modalOverlay.addEventListener('click', () => {
+    modal.classList.remove('is-open');
 });
 
-overlay.addEventListener("click", () => {
-		modal.classList.remove("is-open");
+menuTrigger.addEventListener('click', () => {
+    menuTrigger.classList.toggle('active');
+    nav.classList.toggle('is-open');
+    menuOverlay.classList.toggle('active');
+});
+
+menuOverlay.addEventListener('click', () => {
+    menuTrigger.classList.remove('active');
+    nav.classList.remove('is-open');
+    menuOverlay.classList.remove('active');
 });
