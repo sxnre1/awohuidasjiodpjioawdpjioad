e6f9acd85bdfe359074bf7946f589fa444987f5c
unknown <senleodaaauwa@gmail.com>
2026-03-08 20:24:39 +0900

    .103

--- api/admin-login.js
+++ api/admin-login.js
@@ -1,24 +1,30 @@
-import jwt from "jsonwebtoken";
+const jwt = require('jsonwebtoken');
 
-const ADMIN = { 
-    username: process.env.ADMIN_USER || "", 
-    password: process.env.ADMIN_PASS || "" 
+const ADMIN = {
+    username: process.env.ADMIN_USER || "admin@wonhee-mkt.shop",
+    password: process.env.ADMIN_PASS || "hfNx5@LR#(#*k7f"
 };
+const SECRET_KEY = process.env.JWT_SECRET || "awudihuiaosdaiuodawsduoihasduohnawdojnasdbjinasdhuoi123801238907123#981230830870891308712309812309-1239-213409124";
 
-const SECRET_KEY = process.env.JWT_SECRET || "supersecretkey";
+module.exports = async (req, res) => {
+    try {
+        if(req.method !== "POST") return res.status(405).end();
 
-export default async function handler(req, res) {
-    if(req.method === "POST") {
         const body = await req.json();
         const { username, password } = body;
 
-        if(username === ADMIN.username && password === ADMIN.password) {
+        if (!username || !password) {
+            return res.status(400).json({ status: "fail", message: "아이디와 비밀번호를 입력해주세요." });
+        }
+
+        if (username === ADMIN.username && password === ADMIN.password) {
             const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: "1h" });
             return res.status(200).json({ status: "ok", token });
         } else {
             return res.status(401).json({ status: "fail", message: "아이디 또는 비밀번호가 잘못되었습니다." });
         }
+    } catch (err) {
+        console.error(err); // 로그 확인
+        res.status(500).json({ status: "fail", message: "서버 오류가 발생했습니다." });
     }
-
-    res.status(405).end();
-}
+};
